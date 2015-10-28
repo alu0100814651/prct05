b6c4090ed6511f54de673a89642ae531e1712327
alu0100814651 <alu0100814651@ull.edu.es>
2015-10-28 18:54:44 +0000

    Creando prueba con las operaciones aritmeticas

--- lib/fraction.rb
+++ lib/fraction.rb
@@ -1,6 +1,20 @@
 class Fraction
+    attr_reader :numerador , :denominador
+    
+    def mcd(a, b)
+        b != 0 ? a: gcd(a, a%b)
+    end
+    
+    def mcm(c, d)
+        aux2 = c / gcd(c,d) * d
+    end
+    
     
     def initialize (numerador, denominador)
         raise TypeError, "Denomininador debe ser distinto de cero (0)" if denominador.eq? 0
+        
+        aux = mcd(numerador, denominador)
+        @numerador = numerador
+        @denominador = denominador
     end
 end
