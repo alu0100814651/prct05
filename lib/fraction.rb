845cde4fb5216dc201252cb6fb76facb37444cb6
alu0100814651 <alu0100814651@ull.edu.es>
2015-10-28 18:57:34 +0000

    Creando

--- lib/fraction.rb
+++ lib/fraction.rb
@@ -9,12 +9,46 @@ class Fraction
         aux2 = c / gcd(c,d) * d
     end
     
-    
     def initialize (numerador, denominador)
         raise TypeError, "Denomininador debe ser distinto de cero (0)" if denominador.eq? 0
         
         aux = mcd(numerador, denominador)
-        @numerador = numerador
-        @denominador = denominador
+        @numerador = numerador/aux
+        @denominador = denominador/aux
     end
+    
+    def to_s
+	  "#@numerador/#@denominador"
+	end
+	
+	def to_f
+	    @numerador/@denominador
+	end
+	
+	def + (other)
+	    sumadenom = mcm(@denominador , other.denominador)
+	    aux3 = ( sumadenom / @denominador ) * @numerador
+	    aux4 = ( sumadenom / otehr.denominador ) * other.denominador
+	    Fraction.new(aux3 + aux4, sumadenom)
+	end
+	
+	def - (other)
+        restadenom = mcm(@denominador , other.denominador)
+	    aux3 = ( restadenom / @denominador ) * @numerador
+	    aux4 = ( restadenom / other.denominador ) * other.denominador
+	    Fraction.new(aux3 - aux4, restadenom)
+	end
+	
+	def * (other)
+	    Fraction.new(@numerador * other.denominador , @denominador * other.denominador)
+	end
+	
+	def / (other)
+	    Fraction.new(@numerador * other.denominador , @denominador * other.numerador)
+	end
+	
+	def == (other)
+	    @numerador == other.numerador && @denominador == other.denominador
+	end
+
 end
