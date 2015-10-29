68695eebf9d4023cc62789128cc54cfd701f7f52
alu0100814651 <alu0100814651@ull.edu.es>
2015-10-29 09:17:17 +0000

    Poniendo las pruebas

--- test/tu_fraction.rb
+++ test/tu_fraction.rb
@@ -1,2 +1,15 @@
 require 'lib/fraction'
-require 'Test/Unit'
+
+class TestPoint < Test::Unit::TestCase
+    def setup
+        @div1 = Fraction.new(5,3)
+        @div2 = Fraction.new(1,2)
+    end
+    
+    def test_simple
+        assert_equal("13/6", (@div1 + @div2).to_s)
+        assert_equal("7/6", (@div1 - @div2).to_s)
+        assert_equal("10/3", (@div1 * @div2).to_s)
+        assert_equal("10/3", (@div1 / @div2).to_s)
+    end
+end
